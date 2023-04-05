72f03747c981d97131b2f61fc41f6e3a3c896bb3
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2023-04-05 17:34:18 -0500

    continue

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './css/App.css'
+
+import  NavBar from './components/NavBar/NavBar'
+
+
+function App() {
+  return (
+    <section className="App">
+      <NavBar/>
+    </section>
+  )
+}
+
+export default App
