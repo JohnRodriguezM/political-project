19adac1c6ee7087933eb544030e47f5898949987
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2023-04-05 23:09:20 -0500

    working

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,10 @@
-import {useState} from 'react'
+import { useState, useEffect } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import "./NavBar.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 import {
   SlSocialInstagram,
@@ -10,46 +12,103 @@ import {
   SlSocialFacebook,
 } from "react-icons/sl";
 
-function BasicExample() {
+import logo from "../../assets/img/logo.svg";
 
-  const [activeLink, setActiveLink] = useState('home')
+/*import navIcon1 from '../../assets/img/nav-icon1.svg'
+import navIcon2 from '../../assets/img/nav-icon2.svg'
+import navIcon3 from '../../assets/img/nav-icon3.svg'*/
+
+function BasicExample() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+
+    if (offset > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-          <img src="" alt="Logo" />
+          <img
+            style={{
+              width: "100px",
+            }}
+            src={logo}
+            alt="Logo"
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icons">
-              <a href="">
-                <SlSocialInstagram />
-              </a>
-              <a href="">
-                <SlSocialTwitter />
-              </a>
-              <a href="">
-                <SlSocialFacebook />
-              </a>
-            </div>
-            <button
-              className="vvd"
-              onClick={() => {
-                console.log("Hello World");
-              }}
-            >
-              Vamos a conectar
-            </button>
-          </span>
+        <Navbar.Collapse className="letter" id="basic-navbar-nav ">
+          <motion.div
+            initial={{
+              y: -100,
+
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+            transition={{ duration: 0.8 }}
+          >
+            <Nav className="me-auto">
+              <Nav.Link id="inicio" href="#inicio">
+                Inicio
+              </Nav.Link>
+              <Nav.Link href="#conoceme">Conócome</Nav.Link>
+              <Nav.Link href="#proyectos">Proyectos</Nav.Link>
+              <Nav.Link href="#apoyanos">Apóyanos</Nav.Link>
+              <Nav.Link href="#newsletter">Newsletter</Nav.Link>
+            </Nav>
+          </motion.div>
+          <motion.div
+            initial={{
+              y: -100,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+            transition={{ duration: 0.8 }}
+          >
+            <span className="navbar-text">
+              <div className="social-icons">
+                <a href="">
+                  <SlSocialInstagram />
+                </a>
+                <a href="">
+                  <SlSocialTwitter />
+                </a>
+                <a href="">
+                  <SlSocialFacebook />
+                </a>
+              </div>
+              <button
+                className="vvd"
+                onClick={() => {
+                  console.log("Hello World");
+                }}
+              >
+                Vamos a conectar
+              </button>
+            </span>
+          </motion.div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
